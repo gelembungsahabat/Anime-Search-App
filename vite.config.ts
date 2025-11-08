70f1dbdf6094db21f4d80dd7ecd8b2c9f31c79da
gelembungsahabat <wildanrgssport@gmail.com>
2025-11-08 15:22:21 +0700

    feat: update main page and add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   //change port for production
   preview: {
     port: 4000,
