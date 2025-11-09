c79e00f24c250295b8a16aee8cc0f24d7f0c30ee
gelembungsahabat <wildanrgssport@gmail.com>
2025-11-09 13:09:51 +0700

    fix: fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ function App() {
   return (
     <main>
       <h1>Anime Search App</h1>
-      <div className="card">
-        <input type="text" className="h-10 border rounded-lg p-6" />
+      <div className="flex flex-col items-center gap-4">
+        <label htmlFor="anime-search-input" className="flex-1 opacity-80">
+          Search your favourite anime here:
+        </label>
+        <input
+          id="anime-search-input"
+          type="text"
+          className="flex-1  border rounded-lg py-2 px-6"
+          placeholder="e.g. Kimi no Na wa"
+        />
       </div>
     </main>
   );
