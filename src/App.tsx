380b4bf9882b52867dd08067045cfc1a2154746b
gelembungsahabat <wildanrgssport@gmail.com>
2025-11-09 20:00:56 +0700

    feat: add pagination and add change some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { useDebouncedCallback } from "use-debounce";
-import { useState } from "react";
+import { Activity, useState } from "react";
 import { fetcher } from "./utils";
 import useSWR from "swr";
 import type { AnimeList } from "./types/jikan-types";
 
 function App() {
   const [animeTitle, setAnimeTitle] = useState<string>("");
+  const [pagination, setPagination] = useState<number>(1);
+  const limit = 10;
   const { data, isLoading, error, mutate } = useSWR<AnimeList>(
-    `https://api.jikan.moe/v4/anime?q=${animeTitle}&limit=${5}`,
+    `https://api.jikan.moe/v4/anime?q=${animeTitle}&limit=${limit}&page=${pagination}`,
     fetcher,
     {
       revalidateOnFocus: false,
@@ -22,10 +24,22 @@ function App() {
     mutate();
   }, 250); // 250ms is given interval from the YoPrint's rules
 
+  const prevPage = () => {
+    if (pagination > 0) {
+      setPagination((prev) => prev - 1);
+    }
+  };
+  const nextPage = () => {
+    if (data && pagination < data.pagination.last_visible_page) {
+      setPagination((prev) => prev + 1);
+    }
+  };
   return (
     <main>
       <section>
-        <h1>Anime Search App</h1>
+        <Activity mode={data ? "hidden" : "visible"}>
+          <h1>Anime Search App</h1>
+        </Activity>
         <div className="flex flex-col items-center gap-4">
           <label htmlFor="anime-search-input" className="flex-1 opacity-80">
             Search your favourite anime here:
@@ -42,16 +56,33 @@ function App() {
         </div>
       </section>
       <section>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 border-2 border-gray-400 rounded-3xl m-8">
-          {data?.data.map((val, idx) => {
-            return (
-              <div key={idx} className="m-2">
-                <img src={`${val.images.jpg.image_url}`} alt="" />
-                <h3>{val.title}</h3> <p>{val.synopsis}</p>
-              </div>
-            );
-          })}
-        </div>
+        <Activity mode={data ? "visible" : "hidden"}>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-5 place-items-center border-2 border-gray-400 rounded-3xl m-8">
+            {data?.data.map((val, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="flex flex-col justify-center m-2 w-40"
+                >
+                  <img
+                    src={`${val.images.jpg.image_url}`}
+                    alt="anime image"
+                    className="h-56 max-w-40"
+                  />
+                  <h4>{val.title}</h4>
+                </div>
+              );
+            })}
+          </div>
+          {
+            <div className="flex flex-row justify-center">
+              <button onClick={prevPage}>previous page</button>{" "}
+              <p>{data?.pagination.current_page}</p> ...{" "}
+              <p>{data?.pagination.last_visible_page}</p>
+              <button onClick={nextPage}>next page</button>
+            </div>
+          }
+        </Activity>
       </section>
     </main>
   );
