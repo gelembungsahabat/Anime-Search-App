8408266b61611b46138316228ecb5435bd3c2346
gelembungsahabat <wildanrgssport@gmail.com>
2025-11-10 14:25:51 +0700

    feat: add character list on anime details

--- src/pages/anime-details.tsx
+++ src/pages/anime-details.tsx
@@ -1,26 +1,57 @@
 import { Link, useParams } from "react-router-dom";
 import { fetcher } from "../utils";
 import useSWR from "swr";
-import type { AnimeDetails } from "../types/jikan-types";
+import type { AnimeCharacters, AnimeDetails } from "../types/jikan-types";
 
 export function AnimeDetails() {
   const { id } = useParams();
-  const { data, error, isLoading } = useSWR<AnimeDetails>(
+  const {
+    data: animeDetailsData,
+    error,
+    isLoading,
+  } = useSWR<AnimeDetails>(
     id ? `https://api.jikan.moe/v4/anime/${id}` : null,
+    fetcher
+  );
+  const { data: charaData } = useSWR<AnimeCharacters>(
+    id ? `https://api.jikan.moe/v4/anime/${id}/characters` : null,
     fetcher
   );
 
   if (error) return <p>Error Loading Details..</p>;
-  if (isLoading || !data) return <p>Loading ..</p>;
+  if (isLoading || !animeDetailsData) return <p>Loading ..</p>;
 
-  const anime = data.data;
+  const anime = animeDetailsData.data;
 
   return (
     <div>
-      <Link to="/">Back to list</Link>
+      <Link to="/">Back to Search</Link>
       <h1>{anime.title}</h1>
-      <img src={anime.images.jpg.image_url} alt={anime.title} width={200} />
-      <p>{anime.synopsis}</p>
+      <div className="flex flex-row gap-10">
+        <img
+          className="object-cover"
+          src={anime.images.jpg.image_url}
+          alt={anime.title}
+          height={400}
+        />
+        <p className="text-left">{anime.synopsis}</p>
+      </div>
+
+      <h3>Anime Characters</h3>
+      <div className="grid grid-cols-10">
+        {charaData?.data.map((val, idx) => (
+          <>
+            <div>
+              <img
+                src={val.character.images.jpg.image_url}
+                alt={anime.title}
+                width={100}
+              />
+              <p key={idx}>{val.character.name}</p>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
--- src/types/jikan-types.ts
+++ src/types/jikan-types.ts
@@ -271,3 +271,40 @@ export interface AnimeDetails {
     ];
   };
 }
+
+export interface AnimeCharacters {
+  data: [
+    {
+      character: {
+        mal_id: 0;
+        url: "string";
+        images: {
+          jpg: {
+            image_url: "string";
+          };
+          webp: {
+            image_url: "string";
+            small_image_url: "string";
+          };
+        };
+        name: "string";
+      };
+      role: "string";
+      voice_actors: [
+        {
+          person: {
+            mal_id: 0;
+            url: "string";
+            images: {
+              jpg: {
+                image_url: "string";
+              };
+            };
+            name: "string";
+          };
+          language: "string";
+        }
+      ];
+    }
+  ];
+}
