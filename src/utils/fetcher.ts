ef2f4964a0de8591314916ca4bf1c4e75e140db1
gelembungsahabat <wildanrgssport@gmail.com>
2025-11-10 10:38:42 +0700

    feat: add abort request while typing again

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,10 +1,12 @@
 export const fetcher = async <T>(
-  ...args: Parameters<typeof fetch>
+  url: string,
+  init?: RequestInit
 ): Promise<T> => {
-  const res = await fetch(...args);
+  const res = await fetch(url, init);
   if (!res.ok)
     throw new Error(
-      "You are being rate-limited. Please follow Rate Limiting guidelines: https://docs.api.jikan.moe/#section/Information/Rate-Limiting"
+      `You are being rate-limited. Please follow Rate 
+      Limiting guidelines: https://docs.api.jikan.moe/#section/Information/Rate-Limiting`
     );
   return res.json() as Promise<T>;
 };
